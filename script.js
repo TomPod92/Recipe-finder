a255fa7c9ca1839d3745a1e78a887a6930450e49
Tomek Podsiadlik <podsiadlik.tomek@gmail.com>
2018-06-12 21:30:55 +0200

    new gallery added

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector('.close').addEventListener('click', () => {
+    document.querySelector('.recipe-gallery').classList.toggle("show");
+ 
+});
+
+
+
+document.querySelector('.menu').addEventListener('click', () => {
+    
+    document.querySelector('.recipe-gallery').classList.toggle("show");
+});
+
